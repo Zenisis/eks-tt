bd93ade4b5962422a68a74bb9c9c387b2b3c9195
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-05-11 16:07:01 +0530

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -1,1 +1,7 @@
 #
+#kubectl install commands
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.29.1/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
